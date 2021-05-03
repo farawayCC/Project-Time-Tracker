1a75cc23bf691cffb6ab55e60db31220af6afe97
a.klygin <iosdevfarconsoft@gmail.com>
2021-05-03 15:22:53 +0200

    Init with my electron-react-template

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Project Time Tracker
- 
+# electron-react-teamplate
+Without overengineering
+
+-clone
+- ```yarn```
+- ```yarn build```
+- get your app in /dist
+
+
---
+++ public/electron.js
@@ -0,0 +1,44 @@
+const electron = require("electron");
+const {
+  app,
+  BrowserWindow
+} = electron;
+
+const path = require("path");
+const isDev = require("electron-is-dev");
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 820,
+    height: 680,
+    icon: '../public/icon.ico',
+    webPreferences: {
+      backgroundThrottling: false,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'electron-preload.js')
+    }
+  });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.setAppUserModelId("Your app name")
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
